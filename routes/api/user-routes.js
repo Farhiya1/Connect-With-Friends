2dbd7bcdb60c7fca0174d57bc7e9eeaf9e18cd5b
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 16:42:58 +0100

    post + delete request for user friend  end point

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -16,3 +16,9 @@ router.route("/").get(getAllUsers).post(createUsers);
 
 // Takes you to: /api/users/:id <GET, PUT, DELETE request>
 router.route("/:id").get(getUsersById).put(updateUsers).delete(deleteUsers);
+
+// Takes you to: /api/users/:userId/friends/:friendId <POST, DELETE request>
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+// Exports module router
+module.exports = router;
