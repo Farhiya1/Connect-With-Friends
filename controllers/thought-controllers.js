bcebc8132074b63a7b0b126955f46085e02d13c5
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 21:46:44 +0100

    delete thought by existing thought by id

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -71,3 +71,18 @@ const thoughtsController = {
       .catch((err) => res.json(err));
   },
 
+  // Delete an existing thought by ID
+  deleteThoughts({ params }, res) {
+    Thoughts.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res
+            .status(404)
+            .json({ message: "No thoughts with this particular ID!" });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
